14164c67d98566ddff2577bee20a8e66647d82a3
Npx <cheyprivado@gmail.com>
2023-05-07 00:52:23 -0300

    :hammer: Atualizando main script

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from art import tprint
 
+import pyttsx3
+
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
 from chatterbot.conversation import Statement
 
 from response import generateResponse
+
+# -*- coding: utf-8 -*-
 
 import logging  
 
@@ -27,12 +31,21 @@ print('\nPara finalizar a conversa digite "tchau".\n')
 while True:
     userMessage = input("Você: ")
 
-    messageStatement = Statement(userMessage)
+    messageStatement = Statement(userMessage.capitalize())
 
     response = generateResponse(messageStatement)
 
     if userMessage.upper() == "TCHAU":
         print("Astra: até mais!")
+
+        # en = pyttsx3.init() 
+        # en.say("até mais!")
+        # en.runAndWait()
+
         break
     else:
         print(f"Astra: {response}")
+
+        # en = pyttsx3.init() 
+        # en.say(response)
+        # en.runAndWait()
